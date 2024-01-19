112527bff0aedc60a32b8ca272078fddb7587329
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 02:05:31 -0600

    Modificando IMetodoEstadoReporte Modificando MetodoEstadoReporte
    D-

--- SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoEstadoReporte.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoEstadoReporte.cs
@@ -4,5 +4,18 @@ namespace SigetSystem.Server.Repositorio.MetodoAplicado.Implementacion.Padres
 {
     public class MetodoEstadoReporte : IMetodoEstadoReporte
     {
+        private readonly IMetodoLookupGenerico<EstadoReporte> _repoGenerico;
+
+        public MetodoEstadoReporte(IMetodoLookupGenerico<EstadoReporte> repoGenerico)
+        {
+            _repoGenerico = repoGenerico;
+        }
+
+        public Task<List<EstadoReporte>> ConsultaEstadoReporte()
+        {
+            IQueryable<EstadoReporte> lista = await _repoGenerico.Consulta();
+
+            return await lista.ToListAsync();
+        }
     }
 }
--- SigetSystem.Server/Repositorio/MetodoAplicado/Interfaces/Padres/IMetodoEstadoReporte.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Interfaces/Padres/IMetodoEstadoReporte.cs
@@ -2,5 +2,6 @@
 {
     public interface IMetodoEstadoReporte
     {
+        Task<List<EstadoReporte>> ConsultaEstadoReporte();
     }
 }
