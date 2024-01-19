770d8ec96c62b2a0ee968ff6ec5de7ee6b00bfeb
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 01:58:48 -0600

    Modificando IMetodoComentariosInconformidad Modificando MetodoComentariosInconformidad
    D-

--- SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoComentarioInconformidad.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoComentarioInconformidad.cs
@@ -4,5 +4,18 @@ namespace SigetSystem.Server.Repositorio.MetodoAplicado.Implementacion.Padres
 {
     public class MetodoComentarioInconformidad : IMetodoComentariosInconformidad
     {
+        private readonly IMetodoLookupGenerico<ComentariosInconformidad> _repoGenerico;
+
+        public MetodoComentarioInconformidad(IMetodoLookupGenerico<ComentariosInconformidad> repoGenerico)
+        {
+            _repoGenerico = repoGenerico;
+        }
+
+        public async Task<List<ComentariosInconformidad>> ConsultaComentariosInconformidad()
+        {
+            IQueryable<ComentariosInconformidad> lista = await _repoGenerico.Consulta();
+
+            return await lista.ToListAsync();
+        }
     }
 }
