0c78db7bd5a9bf2461ecc5b57af1cdb429e90a23
mariohdez22 <herzmariohdez@gmail.com>
2024-01-18 23:54:55 -0600

    Agregando controladores de mario

---
+++ SigetSystem.Server/Controllers/EstadoPersonalController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SigetSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoPersonalController : ControllerBase
+    {
+    }
+}
