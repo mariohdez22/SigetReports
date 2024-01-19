c1a78418ba07882c2fc29910b658ecc925b22336
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 11:01:43 -0600

    Agregando -MunicipioInstalacionController
    D-

---
+++ SigetSystem.Server/Controllers/MunicipioInstalacionController.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using AutoMapper;
+using Hangfire.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SigetSystem.Server.Models.Entidades.Padres;
+using SigetSystem.Server.Repositorio.MetodoAplicado.Interfaces.Padres;
+using SigetSystem.Shared.DTOs.Padres;
+using SigetSystem.Shared.MPPs;
+
+namespace SigetSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MunicipioInstalacionController : ControllerBase
+    {
+        public readonly IMetodoMunicipioInstalacion _repo;
+        public readonly ILogger<MunicipioInstalacion> _logger;
+        public readonly IMapper _mapper;
+
+        public MunicipioInstalacionController(IMetodoMunicipioInstalacion repo, ILogger<MunicipioInstalacion> logger, IMapper mapper)
+        {
+            _repo = repo;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet("Consulta")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> ConsultaMunicipioInstalacion()
+        {
+            var _apiResponse = new APIResponse<List<MunicipioInstalacionDTO>>();
+
+            try
+            {
+                _logger.LogInformation("Obteniendo el municipio de instalacion");
+
+                var lista = await _repo.ConsultaMunicipioInstalacion();
+
+                _apiResponse.Resultado = _mapper.Map<List<MunicipioInstalacionDTO>>(lista);
+                _apiResponse.EsExitoso = true;
+                _apiResponse.CodigoEstado = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation("Error en el controlador de municipio");
+
+                _apiResponse.EsExitoso = false;
+                _apiResponse.MensajeError = ex.Message;
+                _apiResponse.MensajesError = new List<string>() { ex.Message };
+            }
+
+            return Ok(_apiResponse);
+        }
+    }
+}
