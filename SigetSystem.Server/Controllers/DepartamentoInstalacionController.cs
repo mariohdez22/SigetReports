81dc6bdc81b5e915692e26e8b894eb3a7ad2a049
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 12:07:57 -0600

    Agregando -DepartamentoInstalacionController -EstadoReporteController
    D-

---
+++ SigetSystem.Server/Controllers/DepartamentoInstalacionController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SigetSystem.Server.Models.Entidades.Padres;
+using SigetSystem.Server.Repositorio.MetodoAplicado.Interfaces.Padres;
+using SigetSystem.Shared.DTOs.Padres;
+using SigetSystem.Shared.MPPs;
+using System.Net;
+
+namespace SigetSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentoInstalacionController : ControllerBase
+    {
+        private readonly ILogger<DepartamentoInstalacionController> _logger;
+        private readonly IMetodoDepartamentoInstalacion _repo;
+        private readonly IMapper _mapper;
+
+        public DepartamentoInstalacionController(ILogger<DepartamentoInstalacionController> logger, IMetodoDepartamentoInstalacion repo, IMapper mapper)
+        {
+            _logger = logger;
+            _repo = repo;
+            _mapper = mapper;
+        }
+
+        [HttpGet("Consulta")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> ConsultaDepartamentoInstalacion()
+        {
+            var _apiResponse = new APIResponse<List<DepartamentoInstalacionDTO>>();
+            try
+            {
+                _logger.LogInformation("Obteniendo los departamentos");
+
+                List<DepartamentoInstalacion> resultado = await _repo.ConsultaDepartamentoInstalacion();
+
+                _apiResponse.Resultado = _mapper.Map<List<DepartamentoInstalacionDTO>>(resultado);
+                _apiResponse.EsExitoso = true;
+                _apiResponse.CodigoEstado = HttpStatusCode.OK;
+            }
+            catch (Exception ex)
+            {
+                _apiResponse.EsExitoso = false;
+                _apiResponse.MensajeError = ex.Message;
+                _apiResponse.MensajesError = new List<string>() { ex.ToString() };
+            }
+
+            return Ok(_apiResponse);
+        }
+    }
+}
---
+++ SigetSystem.Server/Controllers/EstadoReporteController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SigetSystem.Server.Repositorio.MetodoAplicado.Interfaces.Padres;
+using SigetSystem.Shared.DTOs.Padres;
+using SigetSystem.Shared.MPPs;
+
+namespace SigetSystem.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadoReporteController : ControllerBase
+    {
+        public readonly IMetodoEstadoReporte _repo;
+        public readonly ILogger<EstadoReporteController> _logger;
+        public readonly IMapper _mapper;
+
+        public EstadoReporteController(IMetodoEstadoReporte repo, ILogger<EstadoReporteController> logger, IMapper mapper)
+        {
+            _repo = repo;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> ConsultaEstadoReporte()
+        {
+            var _apiResponse = new APIResponse<List<EstadoReporteDTO>>();
+
+            try
+            {
+                _logger.LogInformation("Obteniendo estado de reporte");
+
+                var lista = await _repo.ConsultaEstadoReporte();
+
+                _apiResponse.Resultado = _mapper.Map<List<EstadoReporteDTO>>(lista);
+                _apiResponse.EsExitoso = true;
+                _apiResponse.CodigoEstado = HttpStatusCode.OK;
+
+            }
+            catch (Exception ex)
+            {
+                _apiResponse.EsExitoso = false;
+                _apiResponse.MensajeError = ex.Message;
+                _apiResponse.MensajesError = new List<string>() { ex.ToString() };
+            }
+
+            return Ok(_apiResponse);
+        }
+    }
+}
