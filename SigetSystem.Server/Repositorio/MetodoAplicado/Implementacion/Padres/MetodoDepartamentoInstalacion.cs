55a00413d7b08879deb5bddc73600529a6553c84
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 10:24:33 -0600

    Correciones -MetodoDepartamentoInstalacion -IMetodoDepartamentoInstalacion
    D-

--- SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoDepartamentoInstalacion.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoDepartamentoInstalacion.cs
@@ -1,4 +1,7 @@
-﻿using SigetSystem.Server.Repositorio.MetodoAplicado.Interfaces.Padres;
+﻿using Microsoft.EntityFrameworkCore;
+using SigetSystem.Server.Models.Entidades.Padres;
+using SigetSystem.Server.Repositorio.MetodoAplicado.Interfaces.Padres;
+using SigetSystem.Server.Repositorio.MetodoGenerico.Interfaces;
 
 namespace SigetSystem.Server.Repositorio.MetodoAplicado.Implementacion.Padres
 {
