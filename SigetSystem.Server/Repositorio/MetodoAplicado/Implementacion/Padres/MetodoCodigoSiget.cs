bf533a15fcc55c43ff5f6b6305c5bcccb52c2f24
Dennis Alexander Perez Urquiza <ia.pu100321@ufg.edu.sv>
2024-01-19 01:39:56 -0600

    Modificando IMetodoCodigoSiget Modificando MetodoCodigoSiget
    D-

--- SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoCodigoSiget.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Implementacion/Padres/MetodoCodigoSiget.cs
@@ -4,5 +4,18 @@ namespace SigetSystem.Server.Repositorio.MetodoAplicado.Implementacion.Padres
 {
     public class MetodoCodigoSiget : IMetodoCodigoSiget
     {
+        private readonly IMetodoGenerico<CodigoSiget> _repoGenerico;
+
+        public MetodoCodigoSiget(IMetodoGenerico<CodigoSiget> repoGenerico)
+        {
+            _repoGenerico = repoGenerico;
+        }
+
+        public async Task<List<CodigoSiget>> ConsultaCodigoSiget()
+        {
+            IQueryable<CodigoSiget> lista = await _repoGenerico.Consulta();
+
+            return await lista.ToListAsync();
+        }
     }
 }
--- SigetSystem.Server/Repositorio/MetodoAplicado/Interfaces/Padres/IMetodoCodigoSiget.cs
+++ SigetSystem.Server/Repositorio/MetodoAplicado/Interfaces/Padres/IMetodoCodigoSiget.cs
@@ -2,5 +2,6 @@
 {
     public interface IMetodoCodigoSiget
     {
+        Task<List<CodigoSiget>> ConsultaCodigoSiget();
     }
 }
