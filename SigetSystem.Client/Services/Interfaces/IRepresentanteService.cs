f739add487ec131ed2334f405fc8300d31283894
Josue2704 <josue30759@gmail.com>
2024-02-21 22:17:32 -0600

    representante

--- SigetSystem.Client/Services/Interfaces/IRepresentanteService.cs
+++ SigetSystem.Client/Services/Interfaces/IRepresentanteService.cs
@@ -1,6 +1,20 @@
-﻿namespace SigetSystem.Client.Services.Interfaces
+﻿using SigetSystem.Shared.DTOs.Hijas;
+using SigetSystem.Shared.DTOs.Padres;
+using SigetSystem.Shared.MPPs;
+
+namespace SigetSystem.Client.Services.Interfaces
 {
     public interface IRepresentanteService
     {
+
+        Task<APIResponse<List<RepresentanteDTO>>> MostrarRepresentante(ParametrosPaginacion pp);
+
+        Task<RepresentanteDTO> BuscarRepresentante(int id);
+
+        Task<string> CrearRepresentante(RepresentanteDTO representante);
+
+        Task<string> EditarRepresentante(RepresentanteDTO representante, int id);
+
+        Task<string> EliminarRepresentante(int id);
     }
 }
--- SigetSystem.Client/Services/Servicios/EstadoRepresentanteService.cs
+++ SigetSystem.Client/Services/Servicios/EstadoRepresentanteService.cs
@@ -1,8 +1,32 @@
 ﻿using SigetSystem.Client.Services.Interfaces;
+using SigetSystem.Shared.DTOs.Padres;
+using SigetSystem.Shared.MPPs;
+using System.Net.Http.Json;
 
 namespace SigetSystem.Client.Services.Servicios
 {
     public class EstadoRepresentanteService : IEstadoRepresentanteService
     {
+        private readonly HttpClient _httpClient;
+
+        public EstadoRepresentanteService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<EstadoRepresentanteDTO>> MostrarEstadoRepresentante()
+        {
+            var resultado = await _httpClient.GetFromJsonAsync<APIResponse<List<EstadoRepresentanteDTO>>>("api/EstadoPersonal/Consulta");
+
+            if (resultado!.EsExitoso == true)
+            {
+                List<EstadoRepresentanteDTO> lista = resultado.Resultado;
+                return lista;
+            }
+            else
+            {
+                throw new Exception(resultado.MensajeError);
+            }
+        }
     }
 }
