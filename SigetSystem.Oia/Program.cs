605f37a7cc328f543fc0fb164fc40ae19d3d74b4
Ufg Asu <ia.pu100321@ufg.edu.sv>
2024-03-02 22:24:58 -0600

    Agregando (OIA) -VistaReporteInspeccion -Index -TituloService -MainLayout -MainLayout (CSS) -NavMenu -NavMenu (CSS) -App -App (CSS) -ComponentesGenerales (CSS) -Index (HTML) -Program (CS) -SigetSystem.Oia.csproj (NUGETS) -_Imports
    D-

--- SigetSystem.Oia/Program.cs
+++ SigetSystem.Oia/Program.cs
@@ -1,11 +1,20 @@
+using Blazored.Modal;
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SigetSystem.Oia;
+using SigetSystem.Oia.Services.Servicios;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<TituloService>();
+
+builder.Services.AddSweetAlert2();
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
---
+++ SigetSystem.Oia/Services/Servicios/TituloService.cs
@@ -0,0 +1,14 @@
+﻿namespace SigetSystem.Oia.Services.Servicios
+{
+    public class TituloService
+    {
+        public string Titulo { get; private set; } = "Inicio Siget";
+        public event Action OnTitleChanged;
+
+        public void AgregarTitulo(string titulo)
+        {
+            Titulo = titulo;
+            OnTitleChanged?.Invoke();
+        }
+    }
+}
